7c28c12ffdad71c4c7e652b5db2bb5a0ecb4b978
jonat <yair.vazquez001@gmail.com>
2022-01-27 15:51:15 -0600

    Primer commit con todos los cambios

---
+++ JavaBasicos/src/ConversionDatos.java
@@ -0,0 +1,23 @@
+/*
+public class ConversionDatos {
+	public static void main(String[] args) {
+		byte numeroByte = 10;
+		short numeroShort = numeroByte;
+		int numeroInt = numeroShort;
+		long numeroLong = numeroInt;
+		
+		int otroInt = (int) numeroLong;
+		short otroShort = (short) otroInt;
+		
+		short num1 = 123;
+		short num2 = 7342;
+		byte num3 = 12;
+		byte num4 = 33;
+		
+		short suma = (short) (num1 + num2);
+		byte suma2 = (byte) (num3 + num4);
+		int suma3 = num3 + num4;
+		
+	}
+}*/
+
---
+++ JavaBasicos/src/VariablesTiposDeDatos.java
@@ -0,0 +1,18 @@
+
+public class VariablesTiposDeDatos {
+
+	public static void main(String[] args) {
+		/*
+		String nombre = "Jonathan";
+		char inicial = 'C';
+		boolean dato = true;
+		short suma = 24 + 104;
+		int numero = 1012345612;
+		long numeroLargo = 100_000_000;
+		float primedio = 1232.23232f;
+		double numeroPi = 3.1415934324;
+		
+		
+		*/
+	}
+}
